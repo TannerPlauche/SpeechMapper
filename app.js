4a9457c65def1bb3ae907f690f6095f048b5f938
Tanner Plauché <tannerplauche@Tanners-MacBook-Pro.local>
2018-03-25 18:56:28 -0500

    Remove commented out code and extra files

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 let body = document.querySelector("body");
 let style = body.style;
-
-let model;
-
+let model; // model is global
 let modelLoader = setInterval(trainModel, 300);
 
 function trainModel() {
@@ -10,7 +8,7 @@ function trainModel() {
         console.log(brain);
         model = new brain.NeuralNetwork({
             activation: 'sigmoid', // activation function
-            hiddenLayers: [4],
+            hiddenLayers: [4], // 1 hidden by default
             learningRate: 0.6 // global learning rate, useful when training using streams
         });
         // model.train(trainingData);
@@ -27,49 +25,28 @@ function audioError(err) {
     console.log("audio err: ", err);
 }
 
-// let data = new Float32Array();
 
 function receiveAudio(stream) {
     const display = document.getElementById("display");
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     var context = new AudioContext();
-    // let audioBuffer = context.createBufferSource(stream);
 
     const microphone = context.createMediaStreamSource(stream);
     const filter = context.createBiquadFilter();
-    // const processor = context.createScriptProcessor(1024, 1, 1);
     const processor = context.createScriptProcessor(2048, 1, 1);
-    // const oscillator = context.createOscillator();
     filter.frequency = 400;
-    // oscillator.type = "sine";
-    // microphone -> filter -> destination.
-    // microphone.connect(processor);
-    // processor.connect(filter);
     microphone.connect(filter);
     filter.connect(processor);
     processor.connect(context.destination);
 
     processor.onaudioprocess = function (e) {
-        // console.log(e.inputBuffer.getChannelData(0));
-        // var dft = new DFT(1024, 44100);
-        // dft.forward(e.inputBuffer.getChannelData(0));
-        // var spectrum = dft.spectrum;
-        // var specArry = Array.from(spectrum);
 
         var fft = new FFT(2048, 44100);
         fft.forward(e.inputBuffer.getChannelData(0));
         var spectrum = fft.spectrum;
 
-        // if (spectrum[0] < 0.001) {
-        //     style.backgroundColor = "red";
-        // } else {
-        //     style.backgroundColor = "white";
-        // }
         console.log(model.run(spectrum));
-        console.log(model.run(spectrum));
-        // display.textContent = JSON.stringify(spectrum);
         display.textContent = brain.likely(spectrum, model);
-        // console.log("spectrum: ", spectrum);
     };
 
     function changeFreq() {
@@ -78,7 +55,7 @@ function receiveAudio(stream) {
     }
 
     document.querySelector("#freqChanger").addEventListener("change", changeFreq);
-
+    // TODO: Fix stop button. 
     document.getElementById("stopAudio").addEventListener("click", function () {
         filter.disconnect(processor);
         processor.disconnect(context.destination);
@@ -96,8 +73,3 @@ function getUserMedia() {
         .apply(navigator, arguments);
 }
 
-function getAudio() {
-    getUserMedia({audio: true}, receiveAudio, audioError);
-}
-
-// getAudio();
